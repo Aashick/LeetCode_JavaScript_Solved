e459bbaeee873f4c0de4a33fed41dda0a4c60e5b
Aashick <aashickn@gmail.com>
2023-01-19 23:12:29 +0530

    array

---
+++ 283_Move_Zeroes.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+  let lastNonZeroIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[lastNonZeroIndex] = nums[i];
+      lastNonZeroIndex++;
+    }
+  }
+
+  for (let i = lastNonZeroIndex; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+};
+
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Example 2:
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+*/
